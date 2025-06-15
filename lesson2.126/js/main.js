4759f4f3865dc250713ab9d9765b18e8a2dbeaf0
Maxim <86004837+potapovWebDev@users.noreply.github.com>
2025-06-15 13:38:10 +0300

    Add files via upload

--- lesson2.126/js/main.js
+++ lesson2.126/js/main.js
@@ -110,7 +110,7 @@
 
     // Слайдер-галерея
 
-    const swiper = new Swiper('.gallery__slider', {
+    new Swiper('.gallery__slider', {
 
         spaceBetween: 15,
         slidesPerView: 1.5,
@@ -134,6 +134,34 @@
             },
             1101: {
                 slidesPerView: 4,
+            }
+        }
+    });
+
+    // Слайдер-отзывы
+
+    new Swiper('.testimonials__slider', {
+
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+
+        breakpoints: {
+            901: {
+                slidesPerView: 1.5,
+            },
+            1201: {
+                slidesPerView: 2.1,
             }
         }
     });
