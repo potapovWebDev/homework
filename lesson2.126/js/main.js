da20ca00d9f33ec5ece90e698cba5b26609ed38f
Maxim <86004837+potapovWebDev@users.noreply.github.com>
2025-06-17 14:07:01 +0300

    Add files via upload

--- lesson2.126/js/main.js
+++ lesson2.126/js/main.js
@@ -166,4 +166,10 @@
         }
     });
 
+    // Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
 })()
